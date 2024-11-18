6f88a4d476fd7c3c90c990a13d8d08f1cc754ad4
Matthew Trentacoste <web+git@matttrent.com>
2024-11-17 22:21:35 -0800

    Update image imports and remove unused assets

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection } from 'astro:content';
-import { noteSchema } from '../types/note';
+import { noteSchema } from '@mttypes/note';
 
 // 2. Define your collection(s)
 const noteCollection = defineCollection({ 
